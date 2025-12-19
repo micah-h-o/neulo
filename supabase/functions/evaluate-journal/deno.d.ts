09fb93baea4c32ca53f0bc1ea5a7cd724d7f3fdf
micah-h-o <micah.hayes.mail@gmail.com>
2025-12-19 17:34:31 -0500

    Remove unneeded edge func

--- supabase/functions/evaluate-journal/deno.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="https://deno.land/x/types/index.d.ts" />
-
-declare namespace Deno {
-  export function serve(handler: (req: Request) => Response | Promise<Response>): void;
-  export const env: {
-    get(key: string): string | undefined;
-  };
-}
-
