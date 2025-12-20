884003a373959ce35dde8a5b93ee179b16cb0c9c
micah-h-o <micah.hayes.mail@gmail.com>
2025-12-20 16:20:04 -0500

    Remove API Key requirementA:

--- app/api/health/route.ts
+++ app/api/health/route.ts
@@ -1,12 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    const authHeader = request.headers.get("Authorization");
-    console.log(authHeader, process.env.HEALTH_SECRET)
-    
-    if (authHeader != `Bearer ${process.env.HEALTH_SECRET}`) {
-        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
-
     return NextResponse.json({ message: "OK" }, { status: 200 });
 }
