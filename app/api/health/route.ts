c705c8b642ce69a96980de5ad635bd948f265662
micah-h-o <micah.hayes.mail@gmail.com>
2025-12-20 15:49:16 -0500

    API route added

---
+++ app/api/health/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+    const authHeader = request.headers.get("Authorization");
+
+    if (authHeader !== `Bearer ${process.env.HEALTH_SECRET}`) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    return NextResponse.json({ message: "OK" }, { status: 200 });
+}
