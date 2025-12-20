8e5601bc2a50ef075e44cd886fc4597b87c9bff1
micah-h-o <micah.hayes.mail@gmail.com>
2025-12-20 15:58:39 -0500

    Debug for health rouete

--- app/api/health/route.ts
+++ app/api/health/route.ts
@@ -3,9 +3,10 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
     const authHeader = request.headers.get("Authorization");
 
-    if (authHeader !== `Bearer ${process.env.HEALTH_SECRET}`) {
+    if (authHeader != `Bearer ${process.env.HEALTH_SECRET}`) {
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
+    console.log(authHeader, process.env.HEALTH_SECRET)
 
     return NextResponse.json({ message: "OK" }, { status: 200 });
 }
