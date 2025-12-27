31877fbeb6f544f0013632f12b45b61f38c0e5c8
micah-h-o <micah.hayes.mail@gmail.com>
2025-12-27 00:33:34 -0500

    Disallow product analytics when users are journaling or on the page where they can journal

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,21 +1,31 @@
 'use client'
 
-import { usePathname, useSearchParams } from "next/navigation"
+import { usePathname } from "next/navigation"
 import { useEffect } from "react"
-import { usePostHog } from 'posthog-js/react'
 
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
       person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
       defaults: '2025-11-30'
-    })
-    posthog.capture('my event', { property: 'value' })
-  }, [])
+    });
+
+    if (pathname === "/me/today") {
+      posthog.opt_out_capturing();
+    } else {
+      posthog.opt_in_capturing();
+    }
+
+    // Optionally, you may want to re-run this logic when the route changes.
+    // Remove this event if not desired. The following event is just a placeholder.
+    posthog.capture('my event', { property: 'value' });
+  }, [pathname]);
 
   return (
     <PHProvider client={posthog}>
